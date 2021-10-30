1a87549bcb51ce8364983efc70db6ad2caad31db
Sarah Vo <tsvo@sfu.ca>
2021-10-30 14:44:47 -0700

    Basic menu implemented. Need to put in other activities' classes to be finished.

--- app/src/main/java/ca/cmpt276/parentapp/MainActivity.java
+++ app/src/main/java/ca/cmpt276/parentapp/MainActivity.java
@@ -2,7 +2,9 @@ package ca.cmpt276.parentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchActivityFlipCoin();
+        switchActivityTimeoutTimer();
+        switchActivitySettings();
+    }
+
+    void switchActivityFlipCoin(){
+        Button flipCoinButton = findViewById(R.id.coinFlipButton);
+        flipCoinButton.setOnClickListener(View -> {
+            Intent intent = new Intent(this, flipCoin.class);//TODO: ADD CLASS HERE
+            startActivity(intent);
+        });
+    }
+
+    void switchActivityTimeoutTimer(){
+        Button flipCoinButton = findViewById(R.id.timeoutTimerButton);
+        flipCoinButton.setOnClickListener(View -> {
+            Intent intent = new Intent(this, timeoutTimer.class);//TODO: ADD CLASS HERE
+            startActivity(intent);
+        });
+    }
+
+    void switchActivitySettings(){
+        Button flipCoinButton = findViewById(R.id.settingsButton);
+        flipCoinButton.setOnClickListener(View -> {
+            Intent intent = new Intent(this, settings.class);//TODO: ADD CLASS HERE
+            startActivity(intent);
+        });
     }
 }

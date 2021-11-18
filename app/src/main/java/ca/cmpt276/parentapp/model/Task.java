32483f6c6deef9e9940e5fb9a6c8b7b88651ffe9
sperkash <sperkash@sfu.ca>
2021-11-17 20:07:26 -0800

    Remove unnecessary methods from TaskManager class

--- app/src/main/java/ca/cmpt276/parentapp/model/Task.java
+++ app/src/main/java/ca/cmpt276/parentapp/model/Task.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Task {
 
     private String taskName;
-    private int childIndex = -1;
+    private int whoseTurn = -1;
 
     public Task(String taskName) {
         this.taskName = taskName;
@@ -20,21 +20,21 @@ public class Task {
     }
 
     private int setWhoseTurn(int numChildren) {
-        if (childIndex == -1) {
+        if (whoseTurn == -1) {
             Random rand = new Random();
-            childIndex = rand.nextInt(numChildren);
+            whoseTurn = rand.nextInt(numChildren);
         }
-        else if (childIndex >= numChildren) {
-            childIndex = 0;
+        else if (whoseTurn >= numChildren) {
+            whoseTurn = 0;
         }
         else {
-            childIndex = (childIndex + 1) % numChildren;
+            whoseTurn = (whoseTurn + 1) % numChildren;
         }
-        return childIndex;
+        return whoseTurn;
     }
 
     private int getWhoseTurn() {
-        return childIndex;
+        return whoseTurn;
     }
 
 }
--- app/src/main/java/ca/cmpt276/parentapp/model/TaskManager.java
+++ app/src/main/java/ca/cmpt276/parentapp/model/TaskManager.java
@@ -21,20 +21,12 @@ public  class TaskManager {
         taskList = new ArrayList<>();
     }
 
-    public void addTask(String name){
-        taskList.add(new Task(name));
+    public void addTask(Task task){
+        taskList.add(task);
     }
 
     public void removeTask(int index){
         taskList.remove(index);
-    }
-
-    public void editTask(String taskName, int index) {
-        taskList.get(index).editTaskName(taskName);
-    }
-
-    public String getTaskName(int pos) {
-        return taskList.get(pos).getTaskName();
     }
 
     public ArrayList<Task> getTaskList() {

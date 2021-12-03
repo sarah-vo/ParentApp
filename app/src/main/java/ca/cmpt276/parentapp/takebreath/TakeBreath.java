d0a3b123331b595285a2d7903791c0587c1639a5
heyuanw <heyuanw@sfu.ca>
2021-12-02 20:29:00 -0800

    Redesign heading for Take Breath
    Use +/- button to let the user increase or decrease number of breath, only allow value 1~10. Save the setting to shared preferences.

--- app/src/main/java/ca/cmpt276/parentapp/takebreath/TakeBreath.java
+++ app/src/main/java/ca/cmpt276/parentapp/takebreath/TakeBreath.java
@@ -2,6 +2,7 @@ package ca.cmpt276.parentapp.takebreath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -13,12 +14,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import ca.cmpt276.parentapp.R;
+import ca.cmpt276.parentapp.model.ChildManager;
 
 
 // https://www.pngaaa.com/detail/1843470
+// https://pixabay.com/music/solo-piano-piano-moment-9835/
 public class TakeBreath extends AppCompatActivity {
     int breathCount = 0;
     int breathNum;
@@ -33,6 +39,7 @@ public class TakeBreath extends AppCompatActivity {
         setContentView(R.layout.activity_take_breath);
 
         music = MediaPlayer.create(this, R.raw.piano_moment);
+        getBreathNumFromSharedPreferences();
         setupHeading();
         inhaleStateButton();
     }
@@ -45,6 +52,7 @@ public class TakeBreath extends AppCompatActivity {
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_DOWN: {
+                        Toast.makeText(getApplicationContext(), "Now inhale ...", Toast.LENGTH_LONG).show();
                         music.start();
                         if (breathCount == 0) {
                             breathButton.setText("In");
@@ -83,14 +91,14 @@ public class TakeBreath extends AppCompatActivity {
                             resetButton();
                             seconds = 0;
                         }
-                        //For testing
+//                        For testing
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 exhaleStateButton();
                             }
                         }, 2000);
-                        //Testing code end
+//                        Testing code end
                         break;
                     }
                 }
@@ -104,6 +112,7 @@ public class TakeBreath extends AppCompatActivity {
         breathButton.setText("Out");
         breathButton.setOnTouchListener(null);
         music.start();
+        Toast.makeText(getApplicationContext(), "and exhale.", Toast.LENGTH_LONG).show();
         countDownTimer = new CountDownTimer(seconds, 100) {
             @Override
             public void onTick(long l) {
@@ -144,28 +153,46 @@ public class TakeBreath extends AppCompatActivity {
     }
 
     private void setupHeading() {
-        EditText breathNumInput = findViewById(R.id.etBreathNum);
-        breathNumInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        TextView heading = findViewById(R.id.tvHeading);
+        heading.setText("Let's take " + breathNum + " breaths together");
 
+        Button addBreath = findViewById(R.id.btnAddBreath);
+        addBreath.setOnClickListener( view -> {
+            if (breathNum < 10) {
+                breathNum++;
+                heading.setText("Let's take " + breathNum + " breaths together");
             }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+            else {
+                Toast.makeText(this, "Please select between 1 and 10 breaths", Toast.LENGTH_SHORT).show();
             }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                String temp = breathNumInput.getText().toString();
-                if (!"".equals(temp)) {
-                    breathNum = Integer.parseInt(temp);
-                    Toast.makeText(getApplicationContext(), "Let's take " + breathNum + " breaths together...", Toast.LENGTH_SHORT)
-                            .show();
-                }
+        });
+        Button decreaseBreath = findViewById(R.id.btnRemoveBreath);
+        decreaseBreath.setOnClickListener( view -> {
+            if (breathNum > 0) {
+                breathNum--;
+                heading.setText("Let's take " + breathNum + " breaths together");
+            }
+            else {
+                Toast.makeText(this, "Please select between 1 and 10 breaths", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private void storeBreathNumToSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Shared Preference", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("BreathNum", breathNum);
+        editor.commit();
+    }
+
+    private void getBreathNumFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Shared Preference", MODE_PRIVATE);
+        breathNum = sharedPreferences.getInt("BreathNum", 0);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        storeBreathNumToSharedPreferences();
+    }
 }

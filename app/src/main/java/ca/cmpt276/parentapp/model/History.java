c33dba48c0019712c0c9ced2a89f4bcfcd5d7a38
sperkash <sperkash@sfu.ca>
2021-12-03 20:27:34 -0800

    Implement history class

---
+++ app/src/main/java/ca/cmpt276/parentapp/model/History.java
@@ -0,0 +1,18 @@
+package ca.cmpt276.parentapp.model;
+
+import android.graphics.Bitmap;
+
+public class History {
+    private Child child;
+    private String lastTurnDate;
+
+    public History(Child child, String lastTurnDate) {
+        this.child = child;
+        this.lastTurnDate = lastTurnDate;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+}
